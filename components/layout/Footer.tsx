ec8d81072136c178e622709fa4e3e82f36face28
SueNi2002 <sn456@cornell.edu>
2023-04-30 21:49:06 -0400

    changed footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => (
   <Box as="footer" px={4}>
     <Divider my={4} />
     <Text fontSize="sm" color="subtle">
-      Created by Michelle Li (myl39)
+      Created by Bradley, Sue, Tony
     </Text>
   </Box>
 )
