b0bbe2116dd5821313562f1e103efad4e7307d64
Tony Chen <tc448@cornell.edu>
2023-04-30 21:45:35 -0400

    Added firebase config

--- util/firebase.ts
+++ util/firebase.ts
@@ -2,7 +2,14 @@ import { initializeApp, getApps, getApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
 // TODO: Replace with your own Firebase config
-const firebaseConfig = {}
+const firebaseConfig = {
+  apiKey: "AIzaSyDXO6KEnibmplv-T48e1IR8IdNwF4_AeBc",
+  authDomain: "trends-final-project-4b42b.firebaseapp.com",
+  projectId: "trends-final-project-4b42b",
+  storageBucket: "trends-final-project-4b42b.appspot.com",
+  messagingSenderId: "664756321339",
+  appId: "1:664756321339:web:c8c4e12ed425e18e9ef01e"
+}
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
 
