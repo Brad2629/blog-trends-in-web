e3c4336695d619c8f243058d5e54bc3792aa5ed3
Bradley Paliska <bp355@cornell.edu>
2023-05-03 22:47:01 -0400

    like button

--- components/home/PostItem.tsx
+++ components/home/PostItem.tsx
@@ -4,14 +4,16 @@ import {
   VStack,
   IconButton,
   Text,
-  StackDivider,
   HStack,
+  Box,
 } from "@chakra-ui/react"
 import { collection, deleteDoc, doc, updateDoc } from "firebase/firestore"
 import { PostWithId } from "../../types"
 import { db } from "../../util/firebase"
 import { AiOutlineHeart } from "react-icons/ai"
+import { AiFillHeart } from "react-icons/ai"
 import { useAuth } from "../auth/AuthUserProvider"
+import { useState } from "react"
 
 type Props = {
   readonly post: PostWithId
@@ -19,9 +21,32 @@ type Props = {
 
 const PostItem = ({ post: { id, title, text, likes, owner } }: Props) => {
   const { user } = useAuth()
+  const [liked, setLiked] = useState(false)
+
+  const toggleLike = () => {
+    setLiked(!liked)
+  }
+  const iconFontSize = "24px"
   return (
-    <HStack>
-      <Icon as={AiOutlineHeart} />
+    <Box position="relative">
+      <IconButton
+        aria-label="Like post"
+        icon={
+          liked ? (
+            <AiFillHeart fontSize={iconFontSize} />
+          ) : (
+            <AiOutlineHeart fontSize={iconFontSize} />
+          )
+        }
+        position="absolute"
+        right={4}
+        top={4}
+        zIndex={1}
+        onClick={toggleLike}
+        color={liked ? "red" : undefined}
+        bg="white"
+        boxSize={iconFontSize}
+      />
       <VStack
         style={{
           backgroundColor: "white",
@@ -30,21 +55,31 @@ const PostItem = ({ post: { id, title, text, likes, owner } }: Props) => {
           borderRadius: 10,
         }}
         w="100%"
-        divider={<StackDivider borderColor="black" />}
       >
-        <Text
-          width="800px"
-          style={{
-            fontSize: "30px",
-          }}
-        >
-          {title}
-        </Text>
-        <Text width="1000px">{owner}</Text>
-        <Text width="800px">{text}</Text>
+        <VStack align="start" spacing={0} ml={4}>
+          <Text
+            bgGradient="linear(to-r, cyan.700, purple.500)"
+            bgClip="text"
+            lineHeight={1.33}
+            width="800px"
+            fontSize="30px"
+          >
+            {title}
+          </Text>
+            <Text fontSize="small" color="gray.500">
+              from {owner}
+            </Text>
+          <Box borderTop="1px solid" borderColor="gray.200" pt={1}>
+          <Text fontStyle="italic" mt={2}>
+            {text}
+          </Text>
+          </Box>
+        </VStack>
       </VStack>
-    </HStack>
+    </Box>
   )
 }
 
 export default PostItem
+
+
